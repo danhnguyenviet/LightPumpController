b20fcf40fede5f8c91f9175186f7740588296780
mogonX <11520277uit@gmail.com>
2016-03-10 12:56:13 +0700

    fix checkbox remember login

--- IoT/app/src/main/java/com/danh/iot/MainActivity.java
+++ IoT/app/src/main/java/com/danh/iot/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -56,6 +58,35 @@ public class MainActivity extends AppCompatActivity {
     public void callAboutActivity(View view) {
         Intent intent = new Intent(this, AboutActivity.class);
         startActivity(intent);
+    }
+
+
+    /**
+     * Create menu on action bar
+     * @param menu
+     * @return
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_action_bar,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    /**
+     * Do event when select item on Menu Action Bar
+     * @param item
+     * @return
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.menu.menu_action_bar:
+            {
+
+            }
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     /**
